28ff52b79fa715c6b56de49d2e14c4c5e6beefdf
Deep Dhakate <75447837+InTruder-Sec@users.noreply.github.com>
2023-01-31 05:52:12 +0000

    update

--- resume-builder/src/App.js
+++ resume-builder/src/App.js
@@ -1,25 +1,38 @@
 import './App.css';
 import NavPannel from './NavPannel';
-import DisplayImg from './Images/desktop.svg';
+import FooterDiv from './footer';
+import DisplayImg from './Images/desktop.svg'
 
 function App() {
   return (
     <div className="wrapper">
       <NavPannel />
+      <img className='DesktopImg' id='desktopImg' alt='image' src={DisplayImg}></img>
       <FirstDisplay />
+      <FooterDiv />
     </div>
   );
 }
 
 function FirstDisplay() {
   return (
-    <div className='firstStepx032'>
-      <img className='DesktopImg' alt='image' src={DisplayImg}></img>
-      <div className='details_0x32'>Free Online Resume Builder</div>
-      <div className='details_0x33'>A Quick and Easy Way to Create Your Professional Resume. Choose from over thirty modern and professional templates.  All of which can be customized to your liking. Fast and Easy to Use Our resume builder lets you easily and quickly create a resume using our resume wizard.</div>
-      <button className='startBtn'>Create Resume Now</button>
+    <div className='firstStepx032' id='firstStep_0x32'>
+      <div className='details_0x32' id='details_0x32'>Free Online Resume Builder</div>
+      <div className='details_0x33' id='details_0x33'>A Quick and Easy Way to Create Your Professional Resume. Choose from over thirty modern and professional templates.  All of which can be customized to your liking. Fast and Easy to Use Our resume builder lets you easily and quickly create a resume using our resume wizard.</div>
+      <button className='startBtn' onClick={Step1}>Create Resume Now</button>
     </div>
   )
 }
+
+
+
+function Step1() {
+  const rImg = document.getElementById("desktopImg");
+  document.getElementById('firstStep_0x32').style.opacity=0;
+  rImg.style.right = "40%"
+  
+
+}
+
 
 export default App;
---
+++ resume-builder/src/footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function FooterDiv() {
+    return (
+        <div className="footer">
+            Crafted with ❤ by Deep Dhakate 
+        </div>
+    )
+}
+
+export default FooterDiv;
