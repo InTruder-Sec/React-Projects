b08a491c3b92e22733784561d8574e242ea90693
= <=>
2023-02-17 19:15:58 +0530

    Update

---
+++ resume-builder/src/templates/MainTemplate1.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+function MainTemplate1() {
+    return (
+        <div className="MainTemplate1"> 
+            <div className="mt_01"></div>
+            <div className="f_name">Captain Price</div>
+        </div>
+    )
+}
+
+export default MainTemplate1;
