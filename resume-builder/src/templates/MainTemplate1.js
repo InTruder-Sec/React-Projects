e2544d91b3ea9c9923ad95d64a580b33ec137e73
= <=>
2023-02-18 18:54:44 +0530

    Update

--- resume-builder/src/templates/MainTemplate1.js
+++ resume-builder/src/templates/MainTemplate1.js
@@ -5,11 +5,20 @@ function MainTemplate1() {
     return (
         <div className="MainTemplate1"> 
             <div className="mt_01_temp">
+                <div className="c_details">
+                    <div className="p_no">+91 9022244068</div>
+                    <div className="add_01">
+                        dhakatedeep909@gmail.com
+                    </div>
+                </div>
                 <div className="f_name">Captain Price</div>
                 <div className="p_name">Student</div><br/>
                 <hr className="t_hr_name"/>
                 <div className="about_t_1">
-                    <div className="about_head_1">About Me</div>
+                    <div className="about head">
+                        <div className="profSVG"><svg className="profSVG" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path fill-rule="evenodd" clip-rule="evenodd" d="M16.5 7.063C16.5 10.258 14.57 13 12 13c-2.572 0-4.5-2.742-4.5-5.938C7.5 3.868 9.16 2 12 2s4.5 1.867 4.5 5.063zM4.102 20.142C4.487 20.6 6.145 22 12 22c5.855 0 7.512-1.4 7.898-1.857a.416.416 0 0 0 .09-.317C19.9 18.944 19.106 15 12 15s-7.9 3.944-7.989 4.826a.416.416 0 0 0 .091.317z" fill="#000000"></path></g></svg></div>
+                        <div className="abtDiv"> About Me</div>
+                    </div>
                 </div>
             </div>
             <div className="mt_01"></div>
