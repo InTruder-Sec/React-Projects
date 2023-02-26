b4599bc2a2a709137b090343137925554f54a864
= <dhakatedeep909@gmail.com>
2023-02-26 22:40:23 +0530

    update

--- book-store/src/components/TrendingBooks.js
+++ book-store/src/components/TrendingBooks.js
@@ -1,9 +1,14 @@
 import React from "react";
+import Main from "./main";
 
 
 export default function TrendingBooks() {
     return (
         <div className="trending">
+            <center>
+                <div className="category--head">TRENDING BOOKS</div>
+                <Main />
+            </center>
             
         </div>
     )
--- book-store/src/data/data.js
+++ book-store/src/data/data.js
@@ -1,39 +1,87 @@
-import Image from './../images/logo.png';
+import RomanceImg from './../images/genre-1.webp';
+import ActionImg from './../images/genre-2.webp';
+import MystryImg from './../images/genre-3.webp';
+import FanatsyImg from './../images/genre-4.webp';
+import HFictionImg from './../images/genre-5.webp';
+import HorrorImg from './../images/genre-6.webp';
+import BioImg from './../images/genre-7.webp';
+import ChildImg from './../images/genre-8.webp';
+import AdultImg from './../images/genre-9.webp';
+import LiteraryImg from './../images/genre-10.webp';
+import NonFictionImg from './../images/genre-11.webp';
+import ScienceImg from './../images/genre-12.webp';
+
 
 const data = [
     {
         title: "Perfect",
-        imgUrl: Image,
+        imgUrl: RomanceImg,
         author: "ED Sheren",
         downloadUrl: "https://google.com"
     },
     {
         title: "Alone Times",
-        imgUrl: Image,
+        imgUrl: HorrorImg,
         author: "ED Sheren",
         downloadUrl: "https://google.com"
     },
     {
         title: "Perfect",
-        imgUrl: Image,
+        imgUrl: ScienceImg,
         author: "ED Sheren",
         downloadUrl: "https://google.com"
     },
     {
         title: "Alone Times",
-        imgUrl: Image,
+        imgUrl: NonFictionImg,
         author: "ED Sheren",
         downloadUrl: "https://google.com"
     },
     {
         title: "Perfect",
-        imgUrl: Image,
+        imgUrl: AdultImg,
         author: "ED Sheren",
         downloadUrl: "https://google.com"
     },
     {
         title: "Alone Times",
-        imgUrl: Image,
+        imgUrl: LiteraryImg,
+        author: "ED Sheren",
+        downloadUrl: "https://google.com"
+    },
+    {
+        title: "Perfect",
+        imgUrl: ChildImg,
+        author: "ED Sheren",
+        downloadUrl: "https://google.com"
+    },
+    {
+        title: "Alone Times",
+        imgUrl: BioImg,
+        author: "ED Sheren",
+        downloadUrl: "https://google.com"
+    },
+    {
+        title: "Perfect",
+        imgUrl: HFictionImg,
+        author: "ED Sheren",
+        downloadUrl: "https://google.com"
+    },
+    {
+        title: "Alone Times",
+        imgUrl: FanatsyImg,
+        author: "ED Sheren",
+        downloadUrl: "https://google.com"
+    },
+    {
+        title: "Perfect",
+        imgUrl: MystryImg,
+        author: "ED Sheren",
+        downloadUrl: "https://google.com"
+    },
+    {
+        title: "Alone Times",
+        imgUrl: ActionImg,
         author: "ED Sheren",
         downloadUrl: "https://google.com"
     }
