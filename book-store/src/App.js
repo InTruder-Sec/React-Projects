8fe7fe619d70684450c8bca70cf5879c23767417
= <dhakatedeep909@gmail.com>
2023-02-28 14:11:24 +0530

    update

--- book-store/src/App.js
+++ book-store/src/App.js
@@ -34,12 +34,17 @@ function NavBar() {
 function Hero() {
   return (
     <div className="hero">
-      <img className="backgroundImg" src={BackgroundImg} alt="background"></img>
+      <div className="backgroundDiv">
+        <img className="backgroundImg" src={BackgroundImg} alt="background"></img>
+      </div>
       <div className="hero--details">
         THE<br/> <div className="hero--details2">BOOKMARK.</div>
         <div className="hero--details3">
           FIND! READ! DOWNLOAD! ANY BOOK ANYTIME ANYWHERE...<br/>
-          <input className="searchInpt" placeholder="Search your book..."></input><button className="searchBtn">→</button>
+          <input className="searchInpt" placeholder="Search your book..."></input>
+          <button className="searchBtn">
+            <svg width="20px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M15 15L21 21" stroke="#323232" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M17 10C17 13.866 13.866 17 10 17C6.13401 17 3 13.866 3 10C3 6.13401 6.13401 3 10 3C13.866 3 17 6.13401 17 10Z" stroke="#323232" stroke-width="2"></path> </g></svg>
+          </button>
         </div> 
       </div>
     </div>
