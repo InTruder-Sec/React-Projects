3f04a04b75bd8a0a1442d6a8f4e82079182a577d
= <dhakatedeep909@gmail.com>
2023-03-22 19:13:04 +0530

    Upadte

---
+++ spotify-clone/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Controllers from "./components/controllers";
+import NavBar from "./components/Nav-Bar";
+
+function App() {
+  return (
+    <div className="App">
+      <Controllers />
+      <NavBar />
+    </div>
+  );
+}
+
+export default App;
---
+++ spotify-clone/src/components/controllers.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./controllers.css";
+
+function Controllers() {
+  return (
+    <div className="controllers">
+      <div className="controllers--img"></div>
+    </div>
+  );
+}
+
+export default Controllers;
