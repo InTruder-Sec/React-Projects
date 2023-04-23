c36331115a0ebf0f3223a13f9e94cd84cd05d05b
= <dhakatedeep909@gmail.com>
2023-04-23 10:04:17 +0530

    Update

--- expense-tracker/src/App.js
+++ expense-tracker/src/App.js
@@ -1,19 +1,10 @@
+import React from "react";
+import Login from "./components/login/Login";
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Login />
     </div>
   );
 }
---
+++ expense-tracker/src/components/login/Login.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./login.css";
+
+function Login() {
+  return (
+    <div className="login--main">
+      <center>
+        <div className="login--background">
+          <div className="login--content">
+            <div className="login--head">hello</div>
+          </div>
+          <div className="login--swiper">hell</div>
+        </div>
+      </center>
+    </div>
+  );
+}
+
+export default Login;
